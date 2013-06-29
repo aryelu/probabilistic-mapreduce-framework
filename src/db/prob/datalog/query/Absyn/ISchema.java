9ec4c3d4373e18952237104aff14f1618a8a9e08
Arye Lukashevski <arye.lu@gmail.com>
2013-06-29 10:09:10 +0000

    done reflex + tests done augment + tests

---
+++ src/db/prob/datalog/query/Absyn/ISchema.java
@@ -0,0 +1,14 @@
+package db.prob.datalog.query.Absyn;
+
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: arye
+ * Date: 29/06/13
+ * Time: 11:22
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ISchema {
+    public Set<String> get_attr();
+}
