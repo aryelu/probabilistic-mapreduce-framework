25b6bd1fc372b21e9d0dfd449a9272d5d5b494a5
Arye Lukashevski <arye.lu@gmail.com>
2014-01-07 21:46:49 +0000

    fixed all

--- src/db/prob/datalog/query/FunctionalDependency/FunctionalDependency.java
+++ src/db/prob/datalog/query/FunctionalDependency/FunctionalDependency.java
@@ -65,55 +65,46 @@ public class FunctionalDependency extends IFunctionalDependency implements IFunc
      * set of attributes A, any subset b<A then A->b
      *
      */
-    private Set<FunctionalDependency> reflex_group() {
-        // TODO reflex on group at hand
-        Set<Set<RelationAttribute>> group_power = this.schema_attr_power();
-        Set<Set<RelationAttribute>> group_singleton = new HashSet<Set<RelationAttribute>>();
-        group_singleton.add(this.schema_get_attr());
-
-        Set<FunctionalDependency> reflex_set = produceFdFromSet(this.get_schema(),
-                group_singleton, group_power);
-
-        reflex_set.addAll(produceFdFromSet(this.get_schema(),
-                group_singleton, group_power));
-        return reflex_set;
-    }
-
-    /*
-     * for each L subset of _left_ and R subset of _right_ add _left_ -> L and
-     *  _right_ -> R to reflex_set
-     */
     public Set<FunctionalDependency> reflexivity() {
-        Set<FunctionalDependency> reflex_group = reflex_group();
-        return reflex_group;
+        Set<FunctionalDependency> allReflex = new HashSet<FunctionalDependency>();
+        Set<FunctionalDependency> leftReflex = this.reflexAttribute(left);
+        Set<FunctionalDependency> rightReflex = this.reflexAttribute(right);
+        allReflex.addAll(leftReflex);
+        allReflex.addAll(rightReflex);
+        return allReflex;
     }
+
+    private Set<FunctionalDependency> reflexAttribute(Set<RelationAttribute> attributeSet) {
+        Set<Set<RelationAttribute>> power = IFunctionalDependency.powerSet(attributeSet);
+        Set<Set<RelationAttribute>> singleton = new HashSet<Set<RelationAttribute>>();
+        singleton.add(attributeSet);
+
+        Set<FunctionalDependency> reflexSet = produceFdFromSet(this.get_schema(), singleton, power);
+        return reflexSet;
+    }
+
 
     /*
       if a --> b and c is set of attr then ca --> cb
      */
     public Set<FunctionalDependency> augmentation() {
-        Set<FunctionalDependency> augment_group = augment_group();
-        return augment_group;
+        Set<RelationAttribute> group_power = this.schema_get_attr();
+        Set<FunctionalDependency> augment_set = new HashSet<FunctionalDependency>();
+        // for c in group_power, add c to both sides
+        for (RelationAttribute gamma : group_power) {
+            Set<RelationAttribute> left_with_gamma = new HashSet<RelationAttribute>(left);
+            left_with_gamma.add(gamma);
+            Set<RelationAttribute> right_with_gamma = new HashSet<RelationAttribute>(right);
+            right_with_gamma.add(gamma);
+            FunctionalDependency fd = new FunctionalDependency(this.get_schema(), left_with_gamma, right_with_gamma);
+            augment_set.add(fd);
+        }
+        return augment_set;
     }
 
     @Override
     public Set<FunctionalDependency> transitivity() {
         return new HashSet<FunctionalDependency>();
-    }
-
-    private Set<FunctionalDependency> augment_group() {
-        Set<Set<RelationAttribute>> group_power = this.schema_attr_power();
-        Set<FunctionalDependency> augment_set = new HashSet<FunctionalDependency>();
-        // for c in group_power, add c to both sides
-        for (Set<RelationAttribute> gamma : group_power) {
-            Set<RelationAttribute> left_with_gamma = new HashSet<RelationAttribute>(left);
-            left_with_gamma.addAll(gamma);
-            Set<RelationAttribute> right_with_gamma = new HashSet<RelationAttribute>(right);
-            right_with_gamma.addAll(gamma);
-            FunctionalDependency fd = new FunctionalDependency(this.get_schema(), left_with_gamma, right_with_gamma);
-            augment_set.add(fd);
-        }
-        return augment_set;
     }
 
     @Override
